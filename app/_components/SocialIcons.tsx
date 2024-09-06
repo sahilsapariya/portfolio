41c969beda6726f9cbfee611785a543a78fc5033
Sahil Sapariya <sahilsapariya03@gmail.com>
2024-09-07 03:32:16 +0530

    FIX: build error fix

--- app/_components/SocialIcons.tsx
+++ app/_components/SocialIcons.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import { FaLinkedin, FaGithub, FaInstagram, FaEnvelope } from 'react-icons/fa';
+import React from "react";
+import { FaLinkedin, FaGithub, FaInstagram, FaEnvelope } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 
-const SocialIcons = (props: any) => {
-    return (
-        <div className={`${props.className} p-3 bg-black z-40`}>
-            <div className="flex flex-col space-y-4 text-white">
-                <a href="https://linkedin.com/in/sahilsapariya"><FaLinkedin size={24} /></a>
-                <a href="https://github.com/sahilsapariya"><FaGithub size={24} /></a>
-                <a href="https://leetcode.com/sahil_sapariya"><SiLeetcode size={24} /></a>
-                <a href="mailto:sahilsapariya03@gmail.com"><FaEnvelope size={24} /></a>
-                <a href="https://instagram.com/_sahil_sapariya_03"><FaInstagram size={24} /></a>
-            </div>
-        </div>
-    );
+interface SocialIconsProps {
+  className?: string;
+}
+
+const SocialIcons: React.FC<SocialIconsProps> = (props) => {
+  return (
+    <div className={`${props.className} p-3 bg-black z-40`}>
+      <div className="flex flex-col space-y-4 text-white">
+        <a href="https://linkedin.com/in/sahilsapariya">
+          <FaLinkedin size={24} />
+        </a>
+        <a href="https://github.com/sahilsapariya">
+          <FaGithub size={24} />
+        </a>
+        <a href="https://leetcode.com/sahil_sapariya">
+          <SiLeetcode size={24} />
+        </a>
+        <a href="mailto:sahilsapariya03@gmail.com">
+          <FaEnvelope size={24} />
+        </a>
+        <a href="https://instagram.com/_sahil_sapariya_03">
+          <FaInstagram size={24} />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default SocialIcons;
