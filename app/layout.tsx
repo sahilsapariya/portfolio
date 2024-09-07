0ea54768eb3bed8a0d58c81037f9768e6023d90c
Sahil Sapariya <sahilsapariya03@gmail.com>
2024-09-07 06:35:01 +0530

    FEATURE: about section for desktop

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#ef9d51]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Head from "next/head";
 import NameScroller from "./_components/NameScrolling";
 import Hero from "./_components/Hero";
@@ -14,24 +15,54 @@ export default function Home() {
         />
       </Head>
 
-      <div className="relative h-screen">
-        {/* Scrolling Name */}
-        <div className="h-[200px] lg:h-[250px] leading-[200px] lg:leading-[250px] w-full">
-          <NameScroller />
+      {/* Landing page */}
+      <main className="bg-[#ef9d51] fixed top-0 right-0 left-0 bottom-0 z-0">
+        <div className="relative h-screen">
+          {/* Scrolling Name */}
+          <div className="h-[200px] lg:h-[250px] leading-[200px] lg:leading-[250px] w-full">
+            <NameScroller />
+          </div>
+
+          {/* Hero Section */}
+          <Hero />
+
+          {/* Profile Image */}
+          <div className="absolute bottom-[-1px] right-0 w-[350px] md:w-[500px]">
+            <Image
+              src="/images/profile-picture.png"
+              alt="Sahil"
+              width={500}
+              height={700}
+              className="w-full h-full drop-shadow-custom pointer-events-none z-40"
+            />
+          </div>
         </div>
+      </main>
 
-        {/* Hero Section */}
-        <Hero />
-
-        {/* Profile Image */}
-        <div className="absolute bottom-[-1px] right-0 w-[350px] md:w-[500px]">
-          <Image
-            src="/images/profile-picture.png"
-            alt="Sahil"
-            width={500}
-            height={700}
-            className="w-full h-full drop-shadow-custom pointer-events-none z-40"
-          />
+      {/* ABOUT SECTION */}
+      <div
+        className="bg-black px-10 py-32 absolute rounded-t-lg z-50"
+        style={{ marginTop: "100vh" }}
+      >
+        <div className="flex items-center">
+          <div className="gradient-text w-1/3 text-white text-[72px] leading-none font-bold justify-center p-10 bg-black flex flex-col">
+            <span>About</span>
+            <span className="mt-[-15px]">About</span>
+            <span className="mt-[-15px]">About</span>
+            <span className="mt-[-15px]">About</span>
+          </div>
+          <div className="w-2/3 p-10">
+            <p className="text-white text-[24px]">
+              Sahil <span className="text-[16px] font-light">(he/him)</span> is
+              a skilled Full-Stack Developer passionate about creating
+              cutting-edge web applications. Proficient in React.js, Next.js,
+              TypeScript, Python, Django, and Flask, he excels in building
+              intuitive interfaces and scalable backend systems. His innovation
+              was recognized with a win at DUHACKS 2.0 Hackathon. Sahil is eager
+              to apply his expertise to challenging projects and deliver
+              impactful solutions.
+            </p>
+          </div>
         </div>
       </div>
     </div>
